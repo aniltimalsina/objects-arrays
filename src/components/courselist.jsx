b01757422eaad8b99816552bc64d7c17dfff9901
aniltimalsina <timalsinaanil96@gmail.com>
2023-11-12 18:59:52 -0500

    add the functionlaity of filter

--- src/components/courselist.jsx
+++ src/components/courselist.jsx
@@ -1,18 +1,41 @@
 import courses from "../data/courses.json";
 import "../styles/course.css";
+
+function filterCourse(event) {
+  const filterValue = event.target.value.toLowerCase();
+  //console.log(filterValue);
+
+  const filteredCourses = courses.filter((course) =>
+    course.instructor.toLowerCase().includes(filterValue)
+  );
+  console.log(filteredCourses);
+
+  const currCourseList = document.getElementById("course-list");
+  currCourseList.innerHTML = "";
+
+  filteredCourses.forEach((course) => {
+    const listItem = document.createElement("div");
+    listItem.textContent = `${course.name} - ${course.instructor}`;
+    listItem.classList.add("course-card");
+    currCourseList.append(listItem);
+  });
+}
 function CourseList() {
   return (
     <>
       <h1>Instructor</h1>
-      <div className="course">
+      <input
+        id="instructorFilter"
+        type="text"
+        placeholder="Filter by instructor"
+        onChange={filterCourse}
+      />
+
+      <div className="course" id="course-list">
         {courses.map((course) => {
           return (
             <div key={course.id} className="course-card">
-              {"Course Name = " +
-                course.name +
-                "\n" +
-                "Instructor = " +
-                course.instructor}
+              <strong>{course.name}</strong> - {course.instructor}
             </div>
           );
         })}
